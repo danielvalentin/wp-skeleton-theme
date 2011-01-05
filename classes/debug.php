ba2245362e8df473937dc792e329bd83677b1f40
Tosak <daniel@danwebs.dk>
2011-01-05 10:44:30 +0100

    Updated the debug class to my Kohana version (much better)

--- classes/debug.php
+++ classes/debug.php
@@ -2,130 +2,217 @@
 
 class debug
 {
+	private static $colors = array("green", "red", "blue", "orange", "purple");
+	private static $level = 1;
+	private static $silent = false;
 	
-	public static function dump($var, $silent = false)
+	public static function _dump($value, $start = true, $key = false)
 	{
-		if($silent)
-		{
-			echo '<!--';
-		}
-		echo self::unravel($var);
-		if($silent)
-		{
-			echo '-->';
-		}
+		self::$silent = true;
+		echo "\n<!--\n";
+		echo self::dump($value, $start, $key, true);
+		echo "\n-->\n";
 	}
 	
-	public static function unravel($obj, $start = true)
+	public static function dump($value, $start = true, $key = false, $silent = false)
 	{
 		if($start)
 		{
-			$returner = '<ul>';
+			echo self::open_ul();
+		}
+		
+		if(is_array($value))
+		{
+			echo self::open_li();
+				echo self::get_key_headline($key);
+				echo ' ';
+				echo self::get_item_headline($value);
+				echo self::open_ul();
+					$color = self::get_random_color();
+					foreach($value as $key => $val)
+					{
+						self::dump($val, false, self::output($key, '<span style="color:' . $color . ';">', '</span>', false));
+					}
+				echo self::close_ul();
+			echo self::close_li();
+		}
+		elseif(is_object($value))
+		{
+			echo self::open_li();
+				echo self::get_key_headline($key);
+				echo self::get_item_headline($value);
+				echo self::open_ul();
+					$color = self::get_random_color();
+					foreach((array)$value as $key => $val)
+					{
+						self::dump($val, false, self::output($key, '<span style="color:' . $color . ';">', '</span>'));
+					}
+					if(get_class($value) != 'stdClass')
+					{
+						$reflect = new ReflectionClass($value);
+						$methods = $reflect -> getMethods();
+						echo self::open_li() . self::output('Show class info (' . count($methods) . ' methods)', '<a href="#" onclick="var kid=this.parentNode.childNodes[1];if(kid.style.display==\'block\'){kid.style.display=\'none\';}else{kid.style.display=\'block\';};return false;">', '</a>', false);
+							echo self::open_ul('<ul class="methods" style="display:none;">');
+								echo self::open_li() . self::output('Methods', '<strong>', '</strong>', false);
+									echo self::open_ul();
+									foreach($methods as $m)
+									{
+										echo self::open_li();
+											if($m -> isPrivate())
+											{
+												echo self::output('private', '<span style="color:red;">', '</span> ');
+											}
+											if($m -> isProtected())
+											{
+												echo self::output('protected', '<span style="color:blue;">', '</span> ');
+											}
+											if($m -> isPublic())
+											{
+												echo self::output('public', '<span style="color:green;">', '</span> ');
+											}
+											if($m -> isStatic())
+											{
+												echo self::output('static', '<em>', '</em> ');
+											}
+											echo $m -> name . '(';
+											$params = $m -> getParameters();
+											$numParams = count($params);
+											$i = 1;
+											foreach($params as $param)
+											{
+												echo '<em>' . $param -> getName() . '</em>';
+												if($i < $numParams)
+												{
+													echo ', ';
+												}
+												$i++;
+											}
+											echo ')';
+										echo self::close_li();
+									}
+									echo self::close_ul();
+								echo self::close_li();
+								
+								echo self::open_li() . self::output('Defined in', '<strong>', '</strong>');
+									echo self::open_ul();
+										echo self::open_li() . $reflect -> getFileName() . ' at line ' . $reflect -> getStartLine() . '</li>'; 
+									echo self::close_ul();
+								echo self::close_li();
+						echo self::close_ul();
+					echo self::close_li();
+					}
+			echo self::close_ul();
+			echo self::close_li();
 		}
 		else
 		{
-			$returner = '';
-		}
-		if(is_array($obj))
-		{
-			if($start)
+			echo self::open_li();
+			echo self::get_key_headline($key);
+			echo self::get_item_headline($value);
+			if(is_bool($value))
 			{
-				$returner .= '<li><em>Array (' . count($obj) . ')</em>:<ul>';
+				if($value)
+				{
+					echo 'true';
+				}
+				else
+				{
+					echo 'false';
+				}
 			}
 			else
 			{
-				$returner .= '<ul>';
+				echo $value;
 			}
-			foreach($obj as $key => $val)
-			{
-				if(is_array($val))
-				{
-					$returner .= '<li><em>Array (' .  $key . ')</em>:';
-					$returner .= self::unravel($val, false);
-				}
-				elseif(is_object($val))
-				{
-					$returner .= self::unravel($val);
-				}
-				else
-				{
-					$type = gettype($val);
-					$returner .= '<li><strong>' . $key . '</strong> => (' . $type . ') ' . $val . '</li>';
-				}
-			}
-			$returner .= '</li>';
+			echo self::close_li();
 		}
-		elseif(is_object($obj))
+		
+		if($start)
 		{
-			$classname = get_class($obj);
-			$returner .= '<li><em>Object (' . $classname . ')</em>:<ul>';
-			$objVars = get_object_vars($obj);
-			foreach($objVars as $key => $val)
-			{
-				if(is_array($val) || is_object($val))
-				{
-					$returner .= '<li><strong>' . $key . '</strong>: (' . ((is_object($val)) ? 'object' : 'array') . ' ' . count($val) . ')';
-					$returner .= self::unravel($val, false);
-				}
-				else
-				{
-					$type = gettype($val);
-					$returner .= '<li><strong>' . $key . '</strong>: (' . $type . ') ' . $val . '</li>';
-				}
-			}
-			$methods = get_class_methods($obj);
-			$reflection = new ReflectionClass($obj);
-			$classMethods = $reflection -> getMethods();
-			$returner .= '<li><strong style="color:brown;">Class methods</strong>:' . ((!$classMethods) ? ' <em>None defined</em>' : '') . '<ul style="margin:0;padding:0;">';
-			foreach($classMethods as $m)
-			{
-				$returner .= '<li>';
-				if($m -> isPrivate())
-				{
-					$returner .= '<span style="color:red;">private</span>';
-				}
-				if($m -> isProtected())
-				{
-					$returner .= '<span style="color:blue;">protected</span>';
-				}
-				if($m -> isPublic())
-				{
-					$returner .= '<span style="color:green;">public</span>';
-				}
-				if($m -> isStatic())
-				{
-					$returner .= ' <em>static</em> ';
-				}
-				$returner .= ' <strong>' . $m -> name . '</strong>(';
-				$params = $m -> getParameters();
-				$numParams = count($params);
-				$i = 1;
-				foreach($params as $param)
-				{
-					$returner .= $param -> getName();
-					if($i < $numParams)
-					{
-						$returner .= ', ';
-					}
-					$i++;
-				}
-				$returner .= ')';
-				$returner .= '</li>';
-			}
-			$returner .= '</ul></li>';
-			if($reflection -> getFileName())
-			{
-				$returner .= '<li><strong>Defined in file</strong>: ' .$reflection -> getFileName() . ', <strong>at line</strong>: ' . $reflection -> getStartLine() . '</li>';
-			}
-			$returner .= '</ul></li>';
+			echo self::close_ul();
+		}
+	}
+	
+	private static function get_random_color()
+	{
+		return self::$colors[rand(0,count(self::$colors)-1)];;
+	}
+	
+	private static function open_ul($tag = false)
+	{
+		self::$level++;
+		if(self::$silent)
+		{
+			return "\n" . str_repeat("\t", self::$level) . "{\n";
+		}
+		if($tag)
+		{
+			return $tag;
+		}
+		return '<ul>';
+	}
+	
+	private static function close_ul()
+	{
+		self::$level--;
+		if(self::$silent)
+		{
+			return "\n" . str_repeat("\t", self::$level) . "}\n";
+		}
+		return '</ul>';
+	}
+	
+	private static function open_li()
+	{
+		if(self::$silent)
+		{
+			return str_repeat("\t", self::$level);
+		}
+		return '<li>';
+	}
+	
+	private static function close_li()
+	{
+		if(self::$silent)
+		{
+			return "\n";
+		}
+		return '</li>';
+	}
+	
+	private static function output($value, $opentag = '', $closetag = '', $indent = true)
+	{
+		if(self::$silent)
+		{
+			return (($indent) ? str_repeat("\t", self::$level) . $value : $value);
+		}
+		return $opentag . $value . $closetag;
+	}
+	
+	public static function get_key_headline($key, $hidden = false)
+	{
+		if($key !== false)
+		{
+			return self::output(str_replace(array('*','\0'), '', trim($key)), '<strong>', '</strong> => ');
+		}
+		return '';
+	}
+	
+	private static function get_item_headline($item)
+	{
+		if(is_array($item))
+		{
+			return self::output('Array', '<em>', '</em>: ', false) . ' (' . sizeof($item) . '): ';
+		}
+		elseif(is_object($item))
+		{
+			return self::output('Object', '<em>', '</em>: ', false) . ' (' . get_class($item) . '): ';
 		}
 		else
 		{
-			$type = ucfirst(gettype($obj));
-			$returner .= '<li><em>' . $type . '</em>: ' . $obj . '</li>';
+			$type = gettype($item);
+			return self::output('(' . ucfirst($type) . '): ', '<em>', '</em>', false);
 		}
-		$returner .= '</ul></li>';
-		return $returner;
 	}
 	
 }
