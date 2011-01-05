cbf53a3aa138b8d3ef09250dc8806d62b3c72f41
Tosak <daniel@danwebs.dk>
2011-01-05 09:15:37 +0100

    Metas (still learning git)

--- header.php
+++ header.php
@@ -1,1 +1,7 @@
 <!DOCTYPE html>
+<head profile="http://gmpg.org/xfn/11">
+	
+	<meta charset="utf-8" />
+	<meta name="description" content=" " />	<meta name="keywords" content=" " />
+</head>
+<body>
