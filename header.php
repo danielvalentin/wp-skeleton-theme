1398344e18a6da0fd217ee1aef5708d5fdb9cdaf
Tosak <daniel@danwebs.dk>
2011-01-05 09:25:57 +0100

    Test - not using add

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <head profile="http://gmpg.org/xfn/11">
 	
 	<meta charset="utf-8" />
-	<meta name="description" content=" " />
-	<meta name="keywords" content=" " />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
 </head>
 <body>
