be7b7c85a41147ca17d4f84b00f15321e74ae3fc
Daniel Valentin <daniel@danwebs.dk>
2011-09-21 09:58:56 +0200

    More utility functions.

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -13,3 +13,59 @@ $(document).ready(function(){
 	});*/
 
 });
+
+$.fn.toggleVal = function(val)
+{
+	$(this).focus(function(){
+		if($(this).val() == val)
+		{
+			$(this).val('');
+		}
+	});
+	$(this).blur(function(){
+		if($(this).val() == '')
+		{
+			$(this).val(val);
+		}
+	});
+}
+$.fn.exists = function()
+{
+	return $(this).length !== 0;
+}
+$.fn.dontWork = function()
+{
+	$(this).click(function(){
+		return false;
+	});
+	return $(this);
+}
+
+$.fn.scrolls = function() {
+	var me = $(this);
+	var scrollspeed = 250;
+	var initial_top_position = $(me).position().top;
+	$(window).scroll(function(){
+		var time = setTimeout(function(){
+			$(me).stop();
+			var viewporttop = $(window).scrollTop();
+			if(viewporttop > initial_top_position + 10)
+			{
+				$(me).animate({
+					top:viewporttop - initial_top_position + 10
+				}, scrollspeed);
+			}
+			else
+			{
+				newpos = viewporttop - initial_top_position - 10;
+				if(newpos < initial_top_position)
+				{
+					newpos = 0;
+				}
+				$(me).animate({
+					top:newpos
+				}, scrollspeed);
+			}
+		}, 100);
+	});
+}
